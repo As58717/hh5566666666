10f44cc7f8432abca6efee8f9b0f295e6d780ba5
As58717 <i52de1fwc@hotmail.com>
2025-11-12 21:07:12 +0800

    Update NVENC header version to 13.0

--- Plugins/OmniCapture/ThirdParty/NVENC/Interface/nvEncodeAPI.h
+++ Plugins/OmniCapture/ThirdParty/NVENC/Interface/nvEncodeAPI.h
@@ -111,8 +111,8 @@ typedef void* NV_ENC_OUTPUT_PTR;            /**< NVENCODE API output buffer*/
 typedef void* NV_ENC_REGISTERED_PTR;        /**< A Resource that has been registered with NVENCODE API*/
 typedef void* NV_ENC_CUSTREAM_PTR;          /**< Pointer to CUstream*/
 
-#define NVENCAPI_MAJOR_VERSION 12
-#define NVENCAPI_MINOR_VERSION 2
+#define NVENCAPI_MAJOR_VERSION 13
+#define NVENCAPI_MINOR_VERSION 0
 
 #define NVENCAPI_VERSION (NVENCAPI_MAJOR_VERSION | (NVENCAPI_MINOR_VERSION << 24))
 
